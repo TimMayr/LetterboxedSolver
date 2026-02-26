d98fccb0599f13bb1076d1a95cd5ebae8ab41ae7
Tim Mayr <tim.mayr@gmx.at>
2026-02-26 14:26:17 +0100

    added crossplay

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io::Error;
 use std::{env, fs};
 
@@ -9,7 +9,15 @@ fn main() {
     match game {
         Game::LetterBoxed(g) => {
             let res = g.solve();
-            dbg!(res);
+            println!("{:#?}", res)
+        }
+        Game::SpellingBee(g) => {
+            let res = g.solve();
+            println!("{:#?}", res)
+        }
+        Game::Crossplay(g) => {
+            let res = g.solve();
+            println!("{:?}", res)
         }
         Game::Error => {
             println!("Game does not exist")
@@ -20,6 +28,8 @@ fn main() {
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Game {
     LetterBoxed(LetterBoxed),
+    SpellingBee(SpellingBee),
+    Crossplay(Crossplay),
     Error,
 }
 
@@ -29,10 +39,63 @@ impl From<&[String]> for Game {
 
         match game_name.as_str() {
             "letterboxed" => return Self::LetterBoxed(LetterBoxed::new(&args[1..])),
+            "spellingbee" => return Self::SpellingBee(SpellingBee::new(&args[1..])),
+            "crossplay" => return Self::Crossplay(Crossplay::new(&args[1..])),
             _ => {}
         }
 
         Self::Error
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct SpellingBee {
+    letters: Vec<char>,
+    center: char,
+}
+
+impl SpellingBee {
+    pub fn new(args: &[String]) -> Self {
+        let mut letters = Vec::new();
+        for c in args[1].chars() {
+            letters.push(c.to_ascii_lowercase());
+        }
+
+        let center = args[0].chars().nth(0).unwrap().to_ascii_lowercase();
+        letters.push(center);
+
+        Self { letters, center }
+    }
+
+    pub fn solve(&self) -> Vec<String> {
+        let dict = read_file("./words_big.txt");
+
+        if dict.is_err() {
+            println!("Dictionary wasn't found");
+            panic!();
+        }
+
+        let dict = dict.unwrap();
+
+        let words: Vec<&String> = dict
+            .iter()
+            .filter(|s| {
+                s.contains(self.center)
+                    && s.len() > 8
+                    && Self::contains_only_from(s, &self.letters[..])
+            })
+            .collect();
+
+        let mut res = Vec::new();
+        for x in words {
+            res.push(x.clone());
+        }
+
+        res
+    }
+
+    fn contains_only_from(s: &str, allowed: &[char]) -> bool {
+        s.chars().all(|c| allowed.contains(&c))
     }
 }
 
@@ -170,6 +233,105 @@ impl LetterBoxed {
         }
 
         (chars.len() - used_chars.len()) as u32
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Crossplay {
+    letters: HashMap<char, u32>,
+    pattern: Vec<char>,
+}
+
+impl Crossplay {
+    fn new(args: &[String]) -> Crossplay {
+        let mut allowed_char_counts: HashMap<char, u32> = HashMap::new();
+
+        for c in args[0].chars() {
+            let old = if allowed_char_counts.contains_key(&c) {
+                *allowed_char_counts.get(&c).unwrap()
+            } else {
+                0u32
+            };
+
+            allowed_char_counts.insert(c, old + 1);
+        }
+
+        Self {
+            letters: allowed_char_counts,
+            pattern: args[1].chars().to_owned().collect(),
+        }
+    }
+}
+
+impl Crossplay {
+    pub fn solve(&self) -> Vec<String> {
+        let dict = read_file("./words_big.txt");
+
+        if dict.is_err() {
+            println!("Dictionary wasn't found");
+            panic!();
+        }
+
+        let dict = dict.unwrap();
+        let mut possible_words = Vec::new();
+        let mut res = Vec::new();
+
+        for word in dict {
+            if word.len() == self.pattern.len() {
+                possible_words.push(word)
+            }
+        }
+
+        for word in possible_words {
+            let mut is_possible = true;
+
+            let mut used_letters: HashMap<char, u32> = HashMap::new();
+
+            for (i, c) in self.pattern.iter().enumerate() {
+                if *c != '.' {
+                    is_possible = word.chars().nth(i).unwrap() == *c;
+                }
+            }
+
+            for (i, c) in word.chars().enumerate() {
+                if self.pattern[i] != '.' {
+                    continue;
+                }
+
+                let old = if used_letters.contains_key(&c) {
+                    *used_letters.get(&c).unwrap()
+                } else {
+                    0
+                };
+
+                used_letters.insert(c, old + 1);
+            }
+
+            is_possible &= Self::check_if_subset(&self.letters, &used_letters);
+
+            if is_possible {
+                res.push(word)
+            }
+        }
+
+        res
+    }
+
+    fn check_if_subset(
+        playable_chars: &HashMap<char, u32>,
+        chars_to_play: &HashMap<char, u32>,
+    ) -> bool {
+        let mut blanks = *playable_chars.get(&'*').unwrap_or(&0);
+
+        for (c, i) in chars_to_play {
+            if playable_chars.get(&c).unwrap_or(&0) + blanks < *i {
+                return false;
+            } else if *playable_chars.get(&c).unwrap_or(&0) < *i {
+                blanks -= 1;
+            }
+        }
+
+        true
     }
 }
 
